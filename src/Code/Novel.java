f4ccc4385debbdb436bf1fb677b4a1738af0815a
Mana1422 <manasesvillalobos80@gmail.com>
2024-10-03 16:49:04 -0700

    created the novel class and added the csv file containing the table of inputs from the instructions.

---
+++ src/Code/Novel.java
@@ -0,0 +1,29 @@
+/**
+ * Represents a novel.
+ *
+ * @author Manases Villalobos
+ * @author Darrel Tapilaha
+ * @version 1.0
+ */
+
+public class Novel {
+    private final String title;
+    private final String authorName;
+    private final int yearPublished;
+
+    /**
+     * It creates an object representing
+     * a novel.
+     * @param title represents the title of the novel as a string
+     * @param authorName as string representing the name of the author
+     * @param yearPublished as an int representing the year it was published
+     */
+    public Novel(final String title,
+                 final String authorName,
+                 final int yearPublished)
+    {
+        this.title = title;
+        this.authorName = authorName;
+        this.yearPublished = yearPublished;
+    }
+}
